2e9df06772743c18e5d87ddb9b9cefc1e9b74bca
atulillumina <127104607+atulillumina@users.noreply.github.com>
2023-03-10 17:30:46 +0530

    Create UpdateVersion.ps1

---
+++ Pipeline/PowerShellScripts/UpdateVersion.ps1
@@ -0,0 +1,25 @@
+param(
+    [string]$RelativePath
+)
+
+# Define the full file path
+$name = "manifest.json"
+$file = $RelativePath + $name
+
+# Read the file contents as a JSON object
+$json = Get-Content $file -Raw | ConvertFrom-Json
+
+# Extract the current version number and split it into its components
+$versionParts = $json.version -split '\.'
+
+# Increment the last component of the version number
+$versionParts[-1] = [int]$versionParts[-1] + 1
+
+# Combine the version components into a string
+$newVersion = $versionParts -join '.'
+
+# Update the version number in the JSON object
+$json.version = $newVersion
+
+# Save the updated JSON object back to the file
+$json | ConvertTo-Json -Depth 100 | Set-Content $file
