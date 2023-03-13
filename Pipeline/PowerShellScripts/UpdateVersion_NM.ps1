08906f35d16b40bd1533861ecf640125d7f54ece
NareshMupparaju <68237719+NareshMupparaju@users.noreply.github.com>
2023-03-13 15:38:55 +0530

    Create UpdateVersion_NM.ps1

---
+++ Pipeline/PowerShellScripts/UpdateVersion_NM.ps1
@@ -0,0 +1,20 @@
+param(
+    [string]$RelativePath = "./src"
+)
+
+$jsonfiles = Get-ChildItem -Path $RelativePath -Filter manifesh.json -Recurse
+foreach($file in $jsonfiles)
+{
+    $newContent = Invoke-WebRequest $file.FullName | ConvertFrom-Json
+
+    $oldBuildNumber = $newContent.version
+    $MajorVersion = $oldBuildNumber.split('.')[0]
+    $MinorVersion = $oldBuildNumber.split('.')[1]
+    $PatchVersion = $oldBuildNumber.split('.')[2]
+    $PatchVersionNew = [int]$PatchVersion + 1
+
+    $newBuildNumber = $MajorVersion + '.' + $MinorVersion + '.' + [string]$PatchVersionNew
+    $newContent.version = $newBuildNumber
+
+    ConvertTo-Json -InputObject $newContent -Depth 10 | set-content $file.FullName    
+}
